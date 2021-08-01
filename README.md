6209b205e4946c3459940b1777d263e518bf5466
LorenFiorini <lorenfiorini@gmail.com>
2021-08-01 08:09:41 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# firebase-gtk-web-start-ahs1nw
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/firebase-gtk-web-start-ahs1nw)
---
+++ index.js
@@ -0,0 +1,84 @@
+// Import stylesheets
+import './style.css';
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import firebase from 'firebase/app';
+
+// Add the Firebase products that you want to use
+import 'firebase/auth';
+import 'firebase/firestore';
+
+import * as firebaseui from 'firebaseui';
+
+// Document elements
+const startRsvpButton = document.getElementById('startRsvp');
+const guestbookContainer = document.getElementById('guestbook-container');
+
+const form = document.getElementById('leave-message');
+const input = document.getElementById('message');
+const guestbook = document.getElementById('guestbook');
+const numberAttending = document.getElementById('number-attending');
+const rsvpYes = document.getElementById('rsvp-yes');
+const rsvpNo = document.getElementById('rsvp-no');
+
+var rsvpListener = null;
+var guestbookListener = null;
+
+async function main() {
+  // Add Firebase project configuration object here
+  // var firebaseConfig = {};
+  const firebaseConfig = {
+    apiKey: 'AIzaSyCywO9Y1dImuqux0QnlT-rv7bw-SQ-qtwg',
+    authDomain: 'fir-amazing-web-codelab.firebaseapp.com',
+    projectId: 'fir-amazing-web-codelab',
+    storageBucket: 'fir-amazing-web-codelab.appspot.com',
+    messagingSenderId: '231934865284',
+    appId: '1:231934865284:web:8a975b8aa8c843f5be77d1',
+    measurementId: 'G-1MTC0FZB1K'
+  };
+
+  // firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp(firebaseConfig);
+
+  // FirebaseUI config
+  const uiConfig = {
+    credentialHelper: firebaseui.auth.CredentialHelper.NONE,
+    signInOptions: [
+      // Email / Password Provider.
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+      signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+        // Handle sign-in.
+        // Return false to avoid redirect.
+        return false;
+      }
+    }
+  };
+  /*
+  // Sign in Google -- not sure if it works
+  var provider = new firebase.auth.GoogleAuthProvider();
+  provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+  firebase.auth().useDeviceLanguage();*/
+
+  // const ui = new firebaseui.auth.AuthUI(firebase.auth());
+  const ui = new firebaseui.auth.AuthUI(firebase.auth());
+  /*startRsvpButton.addEventListener('click', () => {
+    ui.start('#firebaseui-auth-container', uiConfig);
+  });*/
+  startRsvpButton.addEventListener('click', () => {
+    if (firebase.auth().currentUser) {
+      firebase.auth().signOut(); // User is signed in; allows user to sign out
+    } else {
+      ui.start('#firebaseui-auth-container', uiConfig); // No user is signed in; allows user to sign in
+    }
+  });
+
+  firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+      startRsvpButton.textContent = 'LogOut';
+    } else {
+      startRsvpButton.textContent = 'LogIn';
+    }
+  });
+}
+main();
